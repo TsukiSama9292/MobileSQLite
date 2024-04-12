18ca52f695012c476b3283f33e7a52e9a7fb37fc
Lin_Xuanyou <134621816+TsukiSama9292@users.noreply.github.com>
2024-04-12 14:25:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,20 +15,20 @@ function csvToJSON(csv) {
 }
 
 // 主函式
-function main() {
+async function main() {
     try {
         const categoryResponse = await fetch('Category_2.csv');
-        const categoryCsv = await categoryResponse.text({ encoding: "UTF-8" });
+        const categoryCsv = await categoryResponse.text();
         const categoryJson = csvToJSON(categoryCsv);
         document.getElementById('json-data').innerHTML += '<h2>Category</h2><pre>' + categoryJson + '</pre>';
 
         const subcategoryResponse = await fetch('Subcategory.csv');
-        const subcategoryCsv = await categoryResponse.text({ encoding: "UTF-8" });
+        const subcategoryCsv = await subcategoryResponse.text();
         const subcategoryJson = csvToJSON(subcategoryCsv);
         document.getElementById('json-data').innerHTML += '<h2>Subcategory</h2><pre>' + subcategoryJson + '</pre>';
 
         const courseResponse = await fetch('Course.csv');
-        const courseCsv = await categoryResponse.text({ encoding: "UTF-8" });
+        const courseCsv = await courseResponse.text();
         const courseJson = csvToJSON(courseCsv);
         document.getElementById('json-data').innerHTML += '<h2>Course</h2><pre>' + courseJson + '</pre>';
     } catch (error) {
