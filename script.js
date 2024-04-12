bf9ad566aaa4545c6b328274780abc3c95c9b9e6
Lin_Xuanyou <134621816+TsukiSama9292@users.noreply.github.com>
2024-04-12 13:49:27 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+// 透過 XMLHttpRequest 讀取 CSV 檔案
+function loadCSV(file, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.open("GET", file, true);
+    xhr.send();
+}
+
+// 將 CSV 資料轉換成 JSON 格式
+function csvToJSON(csv) {
+    var lines = csv.split("\n");
+    var result = [];
+    var headers = lines[0].split(",");
+    for (var i = 1; i < lines.length; i++) {
+        var obj = {};
+        var currentline = lines[i].split(",");
+        for (var j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    return JSON.stringify(result);
+}
+
+// 主函式
+function main() {
+    loadCSV('Category.csv', function(categoryCsv) {
+        var categoryJson = csvToJSON(categoryCsv);
+        document.getElementById('json-data').innerHTML += '<h2>Category</h2><pre>' + categoryJson + '</pre>';
+    });
+
+    loadCSV('Subcategory.csv', function(subcategoryCsv) {
+        var subcategoryJson = csvToJSON(subcategoryCsv);
+        document.getElementById('json-data').innerHTML += '<h2>Subcategory</h2><pre>' + subcategoryJson + '</pre>';
+    });
+
+    loadCSV('Course.csv', function(courseCsv) {
+        var courseJson = csvToJSON(courseCsv);
+        document.getElementById('json-data').innerHTML += '<h2>Course</h2><pre>' + courseJson + '</pre>';
+    });
+}
+
+// 調用主函式
+main();
